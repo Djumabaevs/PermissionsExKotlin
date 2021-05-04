212f0e1bac802f18d4a656aa63bddac0503e6150
Bakyt <djumabaevb@gmail.com>
2021-05-04 16:16:20 +0600

    camera permission

--- app/src/main/java/com/example/permissionsexkotlin/MainActivity.kt
+++ app/src/main/java/com/example/permissionsexkotlin/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.permissionsexkotlin
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.permissionsexkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnCameraPermission.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You have already permission for camera"
+                , Toast.LENGTH_SHORT).show()
+            } else {
+                //Request permission
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            }
+        }
     }
 
     companion object {
